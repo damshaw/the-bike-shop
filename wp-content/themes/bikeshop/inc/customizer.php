e4c4113b42e2b8e66d5fb6ecb3a8bc6c1f50a8e0
Adam Shaw <damshaw@gmail.com>
2020-03-15 12:30:16 +1100

    wp base

---
+++ wp-content/themes/bikeshop/inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * bikeShop Theme Customizer
+ *
+ * @package bikeShop
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function bikeshop_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'bikeshop_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'bikeshop_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'bikeshop_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function bikeshop_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function bikeshop_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function bikeshop_customize_preview_js() {
+	wp_enqueue_script( 'bikeshop-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'bikeshop_customize_preview_js' );
---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'rRIJ6uApRhbDGT0UHuLeYySyaw1E4YJhBmfvr0no83TBuqminzGYJXvAVC1Nysdv');
+define('SECURE_AUTH_KEY',  'LmhS3H94FS5inaeemsHwyAisJaf5eJwxvEVYXumFEo5YEBxM7vvLxSmQGynRGpio');
+define('LOGGED_IN_KEY',    'M2muNTYyo8DNI79zQs9P7AiezurMTo91wmeYdXDxUS1F6DTdyYI66Mbteu5zt7c0');
+define('NONCE_KEY',        'gd9HacFNQHH3Y5KHw22vNyDM3gnuoh6zhPSyWYCKqr0MQ6yKUR5FprHX2F7tu3HD');
+define('AUTH_SALT',        'y3wTfanGIKRv6DogVyPcLfu0XQTT53UxhGgzJRGTJpCpW4PdpyAj5pzEg3nPvBNy');
+define('SECURE_AUTH_SALT', 'byv0mYaQxmDPv3asiUgjzxnhtME9FhamMiSoeohH0RI6Qqd84zs05GTUix52dSLS');
+define('LOGGED_IN_SALT',   'Kz1UoJ5rNm5BT42RDA932QszoHeAYq23EhuPx4haDCLoV6ND6xo7xhD0KJK9ezue');
+define('NONCE_SALT',       'hHh0npsPYyBe3u9SbsN65JtGxdee1rehjVxJwFXgRciQ6MVSoWrDC2I47Wxmf8Kv');
